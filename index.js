6eb115b0b7865234caa8a619d90917c1644ecd7d
nguyenhongson <nguyenhongson20433@gmail.com>
2023-07-15 00:36:06 +0700

    be/config-database

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
+const { default: mongoose } = require("mongoose");
+const app = express();
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 require("dotenv").config();
+mongoose
+  .connect(
+    "mongodb+srv://anhnppd06356:Anh16101982@cluster0.dmqwjwx.mongodb.net/shop"
+  )
+  .then(() => console.log("Connected!"));
 const port = process.env.PORT || 8080;
-
 app.listen(port, () => {
   console.log(`App đang chạy trên port ${port}`);
 });
