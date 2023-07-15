e6b9985212cbcaf3743cea6ad7d294b90a197e66
nguyenhongson <nguyenhongson20433@gmail.com>
2023-07-15 22:08:30 +0700

    be/api-product

---
+++ controllers/productController.js
@@ -0,0 +1,138 @@
+const Product = require("../models/productModel");
+const Category = require("../models/categoryModel");
+const slugify = require("slugify");
+const getAllProduct = async (req, res) => {
+  try {
+    const product = await Product.find();
+    if (!product) {
+      return res.status(400).json({
+        message: "Sản phẩm không tồn tại !",
+      });
+    }
+    return res.json({
+      message: "Lấy sản phẩm thành công !",
+      product,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+const getOneProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id).populate(
+      "categoryId"
+    );
+    if (!product) {
+      return res.status(400).json({
+        message: "Sản phẩm không tồn tại !",
+      });
+    }
+    return res.json({
+      message: "Lấy sản phẩm thành công !",
+      category,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+const addProduct = async (req, res) => {
+  const formData = req.body;
+  if (!formData.name) {
+    return res.status(400).json({
+      success: false,
+      message: "Phải nhập tên sản phẩm!",
+    });
+  }
+  try {
+    const product = await Product.create(req.body);
+    await Category.findByIdAndUpdate(product.categoryId, {
+      $addToSet: { products: product._id },
+    });
+    if (!product) {
+      return res.status(400).json({
+        message: "Sản phẩm không tồn tại !",
+      });
+    }
+    return res.json({
+      message: "Thêm sản phẩm thành công !",
+      product,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndRemove(req.params.id);
+    if (!product) {
+      return res.status(400).json({
+        message: "Sản phẩm không tồn tại !",
+      });
+    }
+    const categoryId = product.categoryId;
+    await Category.findByIdAndUpdate(categoryId, {
+      $pull: { products: product._id },
+    });
+    return res.json({
+      message: "Xóa sản phẩm thành công !",
+      product,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+const updateProduct = async (req, res) => {
+  const id = req.params.id;
+  const formData = req.body;
+  if (!formData.name) {
+    return res.status(400).json({
+      success: false,
+      message: "Phải nhập tên sản phẩm!",
+    });
+  }
+  try {
+    const isName = await Product.findOne({ name: formData.name });
+    if (isName) {
+      return res.status(400).json({
+        success: false,
+        message: "Sản phẩm đã tồn tại!",
+      });
+    }
+    const data = await Product.findById(id);
+    if (!data) {
+      return res.status(404).json({
+        success: false,
+        message: "Không tìm thấy sản phẩm",
+      });
+    } else {
+      data.name = formData.name;
+      data.slug = slugify(formData.name, { lower: true });
+      await data.save();
+      return res.status(200).json({
+        success: true,
+        message: "Cập nhật sản phẩm thành công!",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error,
+    });
+  }
+};
+
+module.exports = {
+  getAllProduct,
+  getOneProduct,
+  updateProduct,
+  addProduct,
+  deleteProduct,
+};
