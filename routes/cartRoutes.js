953736b0352a44298554ae8ee8db91d5272a70d6
nguyenhongson <nguyenhongson20433@gmail.com>
2023-07-26 00:31:47 +0700

    api-get-cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const { authenticate } = require("../middlewares/authenticate");
 const { authorization } = require("../middlewares/authorization");
-const addToCart = require("../controllers/cartController");
+const {
+  addToCart,
+  getAllCart,
+  updateCart,
+} = require("../controllers/cartController");
 const route = express.Router();
 
 const cartRouter = (app) => {
   route.post("/cart", addToCart);
+  route.get("/cart/:userId", getAllCart);
+  route.put("/cart", updateCart);
   return app.use("/api/v1", route);
 };
-module.exports = { cartRouter };
+module.exports = { cartRouter, getAllCart };
