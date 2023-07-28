4ee3238c37d0da21741750515c48b86645363066
anhcoder03 <nguyenphianh161003@gmail.com>
2023-07-28 17:20:02 +0700

    fe/payment

--- index.js
+++ index.js
@@ -8,17 +8,23 @@ const { UserRouter } = require("./routes/authRoutes");
 const searchRouter = require("./routes/searchRoutes");
 const uploadImage = require("./routes/uploadRoutes");
 const { cartRouter } = require("./routes/cartRoutes");
+// const { paymentRouter } = require("./routes/paymentRouter");
+const order = require("./routes/paymentRouter");
 const app = express();
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(cookieParser());
+// Hoặc chỉ cho phép yêu cầu từ một nguồn cụ thể (vd: http://localhost:5173)
+
 app.use(cors());
+
 categoryRouter(app);
 productRouter(app);
 UserRouter(app);
 searchRouter(app);
 uploadImage(app);
 cartRouter(app);
+app.use("/api/v1", order);
 require("dotenv").config();
 mongoose
   .connect(
